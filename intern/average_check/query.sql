5aa5d8d000db85363283cdab09659f3086e4c1c0
Aleksandr Murzin <am@Aleksandrs-MacBook-Air.local>
2023-07-20 22:27:21 +0300

    solve average_check

---
+++ intern/average_check/query.sql
@@ -0,0 +1,8 @@
+select month,
+       avg(check_amount) as avg_check,
+       quantileExactExclusive(0.5) (check_amount) as median_check
+  from (select *,
+              toStartOfMonth(toDateTime(buy_date)) as month
+          from default.view_checks) as a
+ group by month
+
