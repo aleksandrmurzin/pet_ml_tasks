4bfbd4cf1ffc5d5c4655e60c7e308212ed497944
Aleksandr Murzin <am@Aleksandrs-MacBook-Air.local>
2023-07-20 12:52:01 +0300

    del comments

--- intern/valid_emails/solution.py
+++ intern/valid_emails/solution.py
@@ -10,23 +10,3 @@ def valid_emails(strings: List[str]) -> List[str]:
         return valid_email_regex.fullmatch(email)
 
     return [i for i in strings if is_valid_email(i)]
-
-
-# import re
-# from typing import List
-
-
-# def valid_emails(strings: List[str]) -> List[str]:
-#     """Take list of potential emails and returns only valid ones"""
-
-#     valid_email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-
-#     def is_valid_email(email: str) -> bool:
-#         return bool(re.fullmatch(valid_email_regex, email))
-
-#     emails = []
-#     for email in strings:
-#         if is_valid_email(email):
-#             emails.append(email)
-
-#     return emails
