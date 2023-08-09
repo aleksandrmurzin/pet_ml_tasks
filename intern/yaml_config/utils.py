8fb876a3d1dde21a301169ed61a954e46f3e48dc
Aleksandr Murzin <am@Aleksandrs-MacBook-Air.local>
2023-08-09 14:24:57 +0300

    solve yaml

--- intern/yaml_config/utils.py
+++ intern/yaml_config/utils.py
@@ -1,5 +1,6 @@
+import collections
 import yaml
-from collections import defaultdict
+
 
 def yaml_to_env(config_file: str) -> str:
     """_summary_
@@ -12,6 +13,7 @@ def yaml_to_env(config_file: str) -> str:
     """
     config_file = yaml.safe_load(config_file)
     env_file = ""
+
     def rec(data, parent_key=None):
         nonlocal env_file
         for k, v in data.items():
@@ -29,80 +31,49 @@ def env_to_yaml(env_list: str) -> str:
     """_summary_
 
     Args:
-        env_list (str): _description_
+        config_file (str): _description_
 
     Returns:
         str: _description_
     """
-    yaml_list = []
     yaml_file = {}
     env_list = [i for i in env_list.split("\n") if i]
 
+    def is_digit(value):
+        try:
+            value = float(value)
+            return value
+        except ValueError:
+            return False
 
-    def rec(keys, value):
-        nonlocal yaml_file
+    def rec(data):
+        keys = data.split("=")[0]
+        v = data.split("=")[1]
         if len(keys.split(".")) == 1:
-            if value.isdigit():
-                value = int(value)
-            elif value.isdecimal():
-                value = float(value)
-            elif value in ["True", "true"]:
-                value = True
-            elif value in ["False", "false"]:
-                value = False
-            return {keys:value}
-        return {keys.split(".")[0]: rec(".".join(keys.split(".")[1:]), value)}
-    for elem in sorted(env_list):
-        keys = elem.split("=")[0]
-        value = elem.split("=")[1]
-        kv = rec(keys, value)
-        yaml_file.update(kv)
+            if v.isdigit():
+                v = int(v)
+            elif is_digit(v):
+                v = is_digit(v)
+            elif v in ["true", "True"]:
+                v = True
+            elif v in ["false", "False"]:
+                v = False
+            return {keys: v}
+        k = keys.split(".")[0]
+        data = f'{".".join(keys.split(".")[1:])}={v}'
+        return {k: (rec(data))}
 
-    import pdb; pdb.set_trace()
-    yaml_file = yaml.dump(yaml_file)
+    def update(d, upd_d):
+        for k, v in upd_d.items():
+            if isinstance(v, collections.abc.Mapping):
+                d[k] = update(d.get(k, {}), v)
+            else:
+                d[k] = v
+        return d
+
+    for idx, el in enumerate(env_list):
+        env_list[idx] = rec(el)
+    for el in env_list:
+        yaml_file = update(yaml_file, el)
+    yaml_file = yaml.safe_dump(yaml_file)
     return yaml_file
-
-config = """
-
-preprocess_params:
-  sr: 24000
-  spect_params:
-    n_fft: 2048
-    win_length: 1200
-    hop_length: 300
-
-model_params:
-  dim_in: 64
-  style_dim: 64
-  latent_dim: 16
-  num_domains: 20
-  max_conv_dim: 512
-  n_repeat: 4
-  w_hpf: 0
-  F0_channel: 256
-
-loss_params:
-  g_loss:
-    lambda_sty: 1.
-    lambda_cyc: 5.
-    lambda_ds: 1.
-    lambda_norm: 1.
-    lambda_asr: 10.
-    lambda_f0: 5.
-    lambda_f0_sty: 0.1
-    lambda_adv: 2.
-    lambda_adv_cls: 0.5
-    norm_bias: 0.5
-  d_loss:
-    lambda_reg: 1.
-    lambda_adv_cls: 0.1
-    lambda_con_reg: 10.
-  
-  adv_cls_epoch: 50
-  con_reg_epoch: 30
-
-optimizer_params:
-  lr: 0.0001
-"""
-
-config_env_to_yaml = utils
