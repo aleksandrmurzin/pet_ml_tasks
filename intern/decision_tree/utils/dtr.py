78565fef00ae033a0096c986f56d28c4406d0bb8
Aleksandr <aleksandr.murzin@yahoo.com>
2023-08-17 01:10:55 +0300

    solve dtr

--- intern/decision_tree/utils/dtr.py
+++ intern/decision_tree/utils/dtr.py
@@ -43,17 +43,17 @@ class DecisionTreeRegressor:
             / (len(y_left) + len(y_right)))
         return weighted_mean_squared_error
 
-    def _split(self, X: np.array, y: np.array, feature: int) -> float:
+    def _split(self, X: np.array, y: np.array, feature: int) -> tuple[float, float]:
         """Find the best split for a node (one feature)"""
         x = X[:, feature]
         best_mse = np.inf
         _best_threshold = max(x)
-        for i in sorted(set(x))[:-1]:
-            current_mse = self._weighted_mse(x[x <= i], x[x > i])
+        for i in sorted(set(x)):
+            current_mse = self._weighted_mse(y[x <= i], y[x > i])
             if current_mse < best_mse:
                 _best_threshold = i
                 best_mse = current_mse
-        return _best_threshold
+        return _best_threshold, best_mse
 
     def _best_split(self, X: np.array, y: np.array) -> tuple[int, float]:
         """Find the best split for a node (one feature)"""
@@ -61,10 +61,8 @@ class DecisionTreeRegressor:
         best_threshold = np.inf
         best_mse = np.inf
         for i in range(X.shape[1]):
-            current_threshold = self._split(X, y, i)
-            current_mse = self._weighted_mse(
-                X[:, i][X[:, i] <= current_threshold],
-                X[:, i][X[:, i] > current_threshold])
+            current_threshold, current_mse = self._split(X, y, i)
+            print(current_mse, current_threshold, i)
             if current_mse < best_mse:
                 best_mse = current_mse
                 best_feature, best_threshold = i, current_threshold
@@ -75,13 +73,13 @@ class DecisionTreeRegressor:
         """Split a node and return the resulting left and right child nodes."""
 
         if depth >= self.max_depth or X.shape[0] <= self.min_samples_split:
-            return Node(best_f, best_thr, X.shape[0], y.mean(), self._mse(y),
+            return Node(best_f, best_thr, X.shape[0], int(np.round(y.mean(), 0)), self._mse(y),
                         left=None, right=None)
         best_f, best_thr = self._best_split(X, y)
         X_l, y_l = X[X[:, best_f] <= best_thr], y[X[:, best_f] <= best_thr]
         X_r, y_r = X[X[:, best_f] > best_thr], y[X[:, best_f] > best_thr]
-        return Node(best_f, best_thr, X.shape[0], y.mean(), self._mse(y),
+        return Node(best_f, best_thr, X.shape[0], int(np.round(y.mean(), 0)), self._mse(y),
                     self._split_node(X=X_l, y=y_l, depth=depth+1,
                                      best_f=best_f, best_thr=best_thr),
-                    self._split_node(X=X_r, y=y_r, depth=depth+1, best_f=best_f,
-                                     best_thr=best_thr))
+                    self._split_node(X=X_r, y=y_r, depth=depth+1,
+                                     best_f=best_f, best_thr=best_thr))
