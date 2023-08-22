37adde08d16689771151a9ad3e875dd4a60c863e
Aleksandr Murzin <am@Aleksandrs-MacBook-Air.local>
2023-08-22 18:51:00 +0300

    add unsolved tree_to_sql

--- intern/tree_to_sql/test_utils.py
+++ intern/tree_to_sql/test_utils.py
@@ -1,11 +1,21 @@
-from utils import convert_tree_to_json
+from utils import convert_tree_to_json, generate_sql_query
 from sklearn.datasets import make_classification
 from sklearn.tree import DecisionTreeClassifier
-# import pandas as pd
+import pandas as pd
 
 X, y = make_classification(n_samples=100, n_features=5, n_informative=2, n_classes=2)
+X = pd.DataFrame(X, columns=["a","b","c", "d", "e"])
+features = X.columns.to_list()
+clf = DecisionTreeClassifier()
+clf.fit(X, y)
 
-def test_convert_tree_to_json():
-    clf = DecisionTreeClassifier()
-    clf.fit(X, y)
+# def test_convert_tree_to_json():
+#     clf = DecisionTreeClassifier()
+#     clf.fit(X, y)
+#     tree_as_json = convert_tree_to_json(clf)
+
+
+
+def test_generate_sql_query():
     tree_as_json = convert_tree_to_json(clf)
+    sql_query = generate_sql_query(tree_as_json, features)   
--- intern/tree_to_sql/utils.py
+++ intern/tree_to_sql/utils.py
@@ -1,25 +1,30 @@
 import json
 from sklearn.tree import DecisionTreeClassifier
-import numpy as np
-
-
-class NpEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return np.float32(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return super(NpEncoder, self).default(obj)
 
 
 def convert_tree_to_json(tree: DecisionTreeClassifier) -> str:
+    """_summary_
+
+    Args:
+        tree (DecisionTreeClassifier): _description_
+
+    Returns:
+        str: _description_
+    """
     def rec(tree, node_index=0):
+        """_summary_
+
+        Args:
+            tree (_type_): _description_
+            node_index (int, optional): _description_. Defaults to 0.
+
+        Returns:
+            _type_: _description_
+        """
         if (tree.tree_.children_left[node_index] == -1
             and tree.tree_.children_right[node_index] == -1):
-                class_label = int(tree.tree_.value[node_index].argmax())
-                return {"Class Label:": class_label}
+            class_label = int(tree.tree_.value[node_index].argmax())
+            return {"class": class_label}
 
         feature_index = int(tree.tree_.feature[node_index])
         threshold = float(tree.tree_.threshold[node_index])
@@ -27,12 +32,31 @@ def convert_tree_to_json(tree: DecisionTreeClassifier) -> str:
         right_child_index = tree.tree_.children_right[node_index]
 
         return {"feature_index": feature_index,
-                "threshold": np.round(threshold, 4),
+                "threshold": round(threshold, 4),
                 "left": rec(tree, left_child_index),
-                "right": rec(tree, right_child_index)
+                "right": rec(tree, right_child_index),
                 }
 
     tree_as_dict = rec(tree)
-    tree_as_json = json.dumps(tree_as_dict, cls=NpEncoder)
+    tree_as_json = json.dumps(tree_as_dict, indent=" ")
 
     return tree_as_json
+
+
+def generate_sql_query(tree_as_json: str, features: list) -> str:
+    """_summary_
+
+    Args:
+        tree_as_json (str): _description_
+        features (list): _description_
+
+    Returns:
+        str: _description_
+    """
+    tree_as_dict = json.dumps(tree_as_json)
+    sql_query = """CASE\nEND"""
+    def rec(node, sql_query=sql_query):
+        import pdb; pdb.set_trace()
+        sql_query.split("CASE")
+    rec(tree_as_dict,)
+    return sql_query
