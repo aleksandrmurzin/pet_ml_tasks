1c4a3aa2aba9e718752bf4a4ed588041d2d13b0d
Aleksandr <aleksandr.murzin@yahoo.com>
2023-09-06 21:06:08 +0300

    solve junior match groups

--- intern/match_groups/utils.py
+++ intern/match_groups/utils.py
@@ -1,25 +1,33 @@
 from typing import List
 from typing import Tuple
 from collections import defaultdict
-import itertools
 
 
 def extend_matches(pairs: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    """_summary_
+
+    Parameters
+    ----------
+    pairs : List[Tuple[int, int]]
+        _description_
+
+    Returns
+    -------
+    List[Tuple[int, int]]
+        _description_
+    """
     d = defaultdict(list)
     pairs = [tuple(sorted(i)) for i in pairs]
     for i in pairs:
-        el1, el2 = i[0], i[1]
-        d[el1].append(el2)
-        d[el2].append(el1)
-
+        for el in i:
+            d[el] += [e for e in i if e != el]
     for i in d:
         for y in d[i]:
             if y in d:
                 d[i] += d[y]
                 d[i] = list(set(d[i]))
+        if i not in d[i]:
+            d[i].append(i)
 
-    for i in d:
-        if len(d[i]) >= 2:
-            [pairs.append(p) for p in itertools.combinations(d[i], 2)
-             if (p[0],p[1]) not in pairs and (p[1],p[0]) not in pairs]
-    return sorted(pairs)
+    pairs = [tuple(sorted(v)) for _, v in d.items()]
+    return sorted(list(set(pairs)))
